54685294f3f1baefccad5d75291d5e935213be85
KMZ0209 <sedmikrasky0209@gmail.com>
2022-11-27 23:11:37 +0900

    Add ls_1

---
+++ 05.ls/ls_1.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+find_files = Dir.glob('*')
+files = []
+find_files.each_slice(3) do |s|
+  files << s
+end
+
+(0..(files.size - 1)).each do |i|
+  out_files = []
+  (0..(files[i].size - 1)).each do |j|
+    out_files << files[i][j]
+  end
+  puts out_files.join("\t")
+end
