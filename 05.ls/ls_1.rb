8942d212613887075d0460554ac26848f6bccb4e
KMZ0209 <sedmikrasky0209@gmail.com>
2022-12-07 23:14:46 +0900

    update2 ls_1

--- 05.ls/ls_1.rb
+++ 05.ls/ls_1.rb
@@ -1,15 +1,12 @@
-# frozen_string_literal: true
+# frozen_string_literal: true #Rubocop対応
 
 find_files = Dir.glob('*')
-files = []
-find_files.each_slice(3) do |s|
-  files << s
-end
+files = find_files.each_slice(3).to_a
 
 (0..(files.size - 1)).each do |i|
   out_files = []
   (0..(files[i].size - 1)).each do |j|
-    out_files << files[j][i]
+    out_files << files[j][i].ljust(12)
   end
-  puts out_files.join("\t")
+  puts out_files.join
 end
