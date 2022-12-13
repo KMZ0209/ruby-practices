9da2dcc0727d071a533e5e477322e6666a164629
KMZ0209 <sedmikrasky0209@gmail.com>
2022-12-13 21:22:59 +0900

    update3 ls_1

--- 05.ls/ls_1.rb
+++ 05.ls/ls_1.rb
@@ -1,12 +1,21 @@
-# frozen_string_literal: true #Rubocop対応
+# frozen_string_literal: true
 
 find_files = Dir.glob('*')
-files = find_files.each_slice(3).to_a
+files = []
+find_files.each_slice(5) do |s|
+  files << s
+end
+
+files.compact!
+
+max_size = files.map(&:size).max
+files.map! { |it| it.is_a?(Array) ? it.values_at(0...max_size) : it }
+files = files.transpose
 
 (0..(files.size - 1)).each do |i|
   out_files = []
   (0..(files[i].size - 1)).each do |j|
-    out_files << files[j][i].ljust(12)
+    out_files << files[i][j].to_s.ljust(max_size + 10)
   end
   puts out_files.join
 end
