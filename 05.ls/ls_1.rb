8f0596e318b853a3bd78c07bf2f4f94426e6f166
KMZ0209 <sedmikrasky0209@gmail.com>
2022-12-18 11:51:43 +0900

    update ls_1

--- 05.ls/ls_1.rb
+++ 05.ls/ls_1.rb
@@ -1,21 +1,35 @@
 # frozen_string_literal: true
 
 find_files = Dir.glob('*')
+max_size = find_files.map(&:size).max
 files = []
-find_files.each_slice(5) do |s|
+all_files = find_files.size
+n = if (all_files % 3).zero?
+      all_files / 3
+    else
+      all_files / 3 + 1
+    end
+find_files.each_slice(n) do |s|
   files << s
 end
 
-files.compact!
+max = files.max_by(&:size).size
 
-max_size = files.map(&:size).max
-files.map! { |it| it.is_a?(Array) ? it.values_at(0...max_size) : it }
+files = files.each do |a|
+  a << nil while a.size < max
+end
+
 files = files.transpose
+
+files.compact
+
+files.map { |it| it.is_a?(Array) ? it.values_at(0...max_size) : it }
 
 (0..(files.size - 1)).each do |i|
   out_files = []
   (0..(files[i].size - 1)).each do |j|
-    out_files << files[i][j].to_s.ljust(max_size + 10)
+    out_files << files[i][j].to_s.ljust(max_size + 1)
   end
+
   puts out_files.join
 end
