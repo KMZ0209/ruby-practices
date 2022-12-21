f49657b33918e927ebddad69f88ed5dbd73ac4f4
KMZ0209 <sedmikrasky0209@gmail.com>
2022-12-21 23:52:37 +0900

    update ls_1

--- 05.ls/ls_1.rb
+++ 05.ls/ls_1.rb
@@ -1,35 +1,27 @@
 # frozen_string_literal: true
 
-find_files = Dir.glob('*')
-max_size = find_files.map(&:size).max
+file_list = Dir.glob('*')
+max_value = file_list.map(&:size).max
 files = []
-all_files = find_files.size
-n = if (all_files % 3).zero?
-      all_files / 3
+number_of_files = file_list.size
+n = if (number_of_files % 3).zero?
+      number_of_files.to_f / 3
     else
-      all_files / 3 + 1
+      number_of_files.to_f / 3 + 1.ceil
     end
-find_files.each_slice(n) do |s|
-  files << s
+files = file_list.each_slice(n).to_a
+
+most_common_elements = files.max_by(&:size).size
+files = files.each do |file_element|
+  file_element << nil while file_element.size < most_common_elements
 end
 
-max = files.max_by(&:size).size
-
-files = files.each do |a|
-  a << nil while a.size < max
-end
-
-files = files.transpose
-
-files.compact
-
-files.map { |it| it.is_a?(Array) ? it.values_at(0...max_size) : it }
-
-(0..(files.size - 1)).each do |i|
+transposed_file = files.transpose
+transposed_file.map { |each_file| [each_file] }
+(0..(transposed_file.size - 1)).each do |i|
   out_files = []
-  (0..(files[i].size - 1)).each do |j|
-    out_files << files[i][j].to_s.ljust(max_size + 1)
+  (0..(transposed_file[i].size - 1)).each do |j|
+    out_files << transposed_file[i][j].to_s.ljust(max_value + 1)
   end
-
   puts out_files.join
 end
