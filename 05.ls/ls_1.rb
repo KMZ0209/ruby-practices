9492bac248240d871b978a624d8a9dc1cdbe1898
KMZ0209 <sedmikrasky0209@gmail.com>
2022-12-25 02:11:27 +0900

    update ls_1

--- 05.ls/ls_1.rb
+++ 05.ls/ls_1.rb
@@ -1,27 +1,21 @@
 # frozen_string_literal: true
 
-file_list = Dir.glob('*')
-max_value = file_list.map(&:size).max
-files = []
-number_of_files = file_list.size
-n = if (number_of_files % 3).zero?
-      number_of_files.to_f / 3
-    else
-      number_of_files.to_f / 3 + 1.ceil
-    end
-files = file_list.each_slice(n).to_a
+files = Dir.glob('*')
+max_file_size = files.map(&:size).max
 
-most_common_elements = files.max_by(&:size).size
-files = files.each do |file_element|
-  file_element << nil while file_element.size < most_common_elements
+file_count = files.size
+column_count = (file_count.to_f / 3).ceil
+nested_files = files.each_slice(column_count).to_a
+row_count = nested_files.max_by(&:size).size
+nested_files.each do |file_element|
+  file_element << nil while file_element.size < row_count
 end
 
-transposed_file = files.transpose
-transposed_file.map { |each_file| [each_file] }
-(0..(transposed_file.size - 1)).each do |i|
+transposed_file = nested_files.transpose
+transposed_file.each do |nested_row|
   out_files = []
-  (0..(transposed_file[i].size - 1)).each do |j|
-    out_files << transposed_file[i][j].to_s.ljust(max_value + 1)
+  nested_row.each do |transposed_element|
+    out_files << transposed_element.to_s.ljust(max_file_size + 1)
   end
   puts out_files.join
 end
