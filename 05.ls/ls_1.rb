a0b7f4fbce27a242e9e24fa3fdbea102e3251a69
KMZ0209 <sedmikrasky0209@gmail.com>
2022-12-28 00:17:07 +0900

    update ls_1

--- 05.ls/ls_1.rb
+++ 05.ls/ls_1.rb
@@ -1,25 +1,22 @@
 # frozen_string_literal: true
 
 files = Dir.glob('*')
-sort_files = files
-max_file_size = sort_files.map(&:size).max
+max_file_size = files.map(&:size).max
 
-file_count = sort_files.size
+file_count = files.size
 column_count = (file_count.to_f / 3).ceil
 
-all_files = sort_files
-nested_files = all_files.each_slice(column_count).to_a
+nested_files = files.each_slice(column_count).to_a
 
- row_count = nested_files.max_by(&:size).size
- nested_files.each do |file_element|
-   file_element << nil while file_element.size < row_count
- end
+row_count = nested_files.max_by(&:size).size
+nested_files.each do |file_element|
+  file_element << nil while file_element.size < row_count
+end
 
- transposed_file = nested_files.transpose
-  transposed_file.each do |file_names|
-    out_files = []
-    file_names.each do |file_name|
-      out_files << file_name.to_s.ljust(max_file_size + 1)
-    end
-    puts out_files.join
+transposed_files = nested_files.transpose
+transposed_files.each do |file_names|
+  file_names.each do |file_name|
+    print file_name.to_s.ljust(max_file_size + 1)
   end
+  print "\n"
+end
