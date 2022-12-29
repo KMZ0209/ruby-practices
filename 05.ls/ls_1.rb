fb56bb15efca0773ea1b11a195d41a21e46dcd5f
KMZ0209 <sedmikrasky0209@gmail.com>
2022-12-29 23:23:04 +0900

    update ls_1

--- 05.ls/ls_1.rb
+++ 05.ls/ls_1.rb
@@ -10,14 +10,14 @@ nested_files = files.each_slice(column_count).to_a
 
 row_count = nested_files.max_by(&:size).size
 nested_files.each do |file_elements|
-  file_element << nil while file_elements.size < row_count
+  file_elements << nil while file_elements.size < row_count
 end
 
 transposed_files = nested_files.transpose
 transposed_files.each do |file_names|
   file_names.each do |file_name|
-    max_file_count = max_file_size
-    print file_name.to_s.ljust(max_file_count + 1)
+    count = max_file_size
+    print file_name.to_s.ljust(count + 1)
   end
   print "\n"
 end
