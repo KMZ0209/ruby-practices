6161b4333be344d17a9a2059bdac9f476d54a84c
KMZ0209 <sedmikrasky0209@gmail.com>
2022-12-31 03:17:21 +0900

    update ls_1

--- 05.ls/ls_1.rb
+++ 05.ls/ls_1.rb
@@ -1,23 +1,27 @@
 # frozen_string_literal: true
 
-files = Dir.glob('*')
-max_file_size = files.map(&:size).max
-
-file_count = files.size
-column_count = (file_count.to_f / 3).ceil
-
-nested_files = files.each_slice(column_count).to_a
-
-row_count = nested_files.max_by(&:size).size
-nested_files.each do |file_elements|
-  file_elements << nil while file_elements.size < row_count
-end
-
-transposed_files = nested_files.transpose
-transposed_files.each do |file_names|
-  file_names.each do |file_name|
-    count = max_file_size
-    print file_name.to_s.ljust(count + 1)
+def print_files_count_in_columns(column_count: 3)
+  files = Dir.glob('*')
+  file_count = files.size
+  column_count = (file_count.to_f / column_count).ceil
+  nested_files = files.each_slice(column_count).to_a
+  row_count = nested_files.max_by(&:size).size
+  nested_files.each do |file_names|
+    file_names << nil while file_names.size < row_count
   end
-  print "\n"
+  max_file_size = files.map(&:size).max
+  transposed_files = nested_files.transpose
+  transposed_files.each do |file_names|
+    file_names.each do |file_name|
+      count = max_file_size
+      print file_name.to_s.ljust(count + 1)
+    end
+    print "\n"
+  end
 end
+
+def file_names_and_max_file_size
+  files = Dir.glob('*')
+  files.max_by(&:size)
+end
+print_files_count_in_columns
