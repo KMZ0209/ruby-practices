3e55bf73de15c55d32b5bc8e6bb9a1d4e498dce3
KMZ0209 <sedmikrasky0209@gmail.com>
2023-01-17 15:18:02 +0900

    lsコマンド4を自主的に修正しました

--- 05.ls/ls_1.rb
+++ 05.ls/ls_1.rb
@@ -22,24 +22,24 @@ PERMISSION_TYPE = {
 def main
   files = Dir.glob('*')
   output_total_block_count(files)
-  output_files(files)
+  output_file_data(files)
 end
 
 def output_total_block_count(files)
   total_block_count = 0
-  files.each do |file|
-    stat_file = File.lstat(file)
-    total_block_count += (stat_file.blksize / 1024).truncate
+  files.each do |file_count|
+    stat_file = File.lstat(file_count)
+    total_block_count += stat_file.blocks
   end
   puts "total #{total_block_count}"
 end
 
-def output_file_type(stat_file)
+def file_type(stat_file)
   file_type = stat_file.ftype
   FILE_TYPE[file_type]
 end
 
-def output_permission(stat_file)
+def permission(stat_file)
   permission_count = stat_file.mode.to_s(8).slice(-3, 3)
   permission_type = permission_count.split('').map do |file_count|
     PERMISSION_TYPE[file_count]
@@ -47,19 +47,19 @@ def output_permission(stat_file)
   permission_type.join('').to_s.ljust(8)
 end
 
-def output_various(stat_file)
-  " #{stat_file.nlink} #{Etc.getpwuid(stat_file.uid).name} #{Etc.getgrgid(stat_file.gid).name} #{stat_file.size.to_s.rjust(4)}"
-end
-
-def output_time_stamp(stat_file)
-  last_updated_time = stat_file.mtime
-  " #{last_updated_time.strftime('%b %d %H:%M')} "
-end
-
-def output_files(files)
+def output_file_data(files)
   files.each do |file|
     stat_file = File.stat(file)
-    puts "#{output_file_type(stat_file)}#{output_permission(stat_file)}#{output_various(stat_file)}#{output_time_stamp(stat_file)}#{file} "
+    output_data = [
+      file_type(stat_file) + permission(stat_file),
+      stat_file.nlink,
+      Etc.getpwuid(stat_file.uid).name,
+      Etc.getgrgid(stat_file.gid).name,
+      stat_file.size.to_s.rjust(4),
+      stat_file.mtime.strftime('%b %d %H:%M'),
+      file
+    ]
+    puts output_data.join(' ')
   end
 end
 
