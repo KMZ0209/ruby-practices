6fa15fdd781294438a93f9459be18df48dfc4710
KMZ0209 <sedmikrasky0209@gmail.com>
2023-03-25 12:39:22 +0900

    mainメソッド内にコードを追記しました

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,33 +4,33 @@ require 'optparse'
 
 def main
   options = ARGV.getopts('lwc')
+  options = { 'l' => true, 'w' => true, 'c' => true } if options.values.none?
   files = ARGV
-  file_data = ARGV.select { |arg| File.file?(arg) } || $stdin.read
-  option = options['l'] || options['w'] || options['c']
-  select_file_data(file_data, files, options, option)
+  file_data = ARGV.select { |filename| File.file?(filename) } || $stdin.read
+  select_file_data(file_data, files, options)
 end
 
-def select_file_data(file_data, files, options, option)
+def select_file_data(file_data, files, options)
   if file_data.empty?
-    output_standard_input_count(options, option)
+    output_standard_input_count(options)
   elsif file_data.length == 1
-    output_files_count(file_data, options, option)
+    output_files_count(file_data, options)
   elsif file_data.length >= 2
-    output_multi_file_options_count(files, options, option)
+    output_multi_file_options_count(files, options)
   end
 end
 
-def output_standard_input_count(options, option)
+def output_standard_input_count(options)
   input = $stdin.read
-  print line_count(input).to_s.rjust(8) if !option || options['l']
-  print word_count(input).to_s.rjust(8) if !option || options['w']
-  print byte_count(input).to_s.rjust(8) if !option || options['c']
+  print line_count(input).to_s.rjust(8) if options['l']
+  print word_count(input).to_s.rjust(8) if options['w']
+  print byte_count(input).to_s.rjust(8) if options['c']
 end
 
-def output_files_count(file_data, options, option)
-  output_line_count(file_data) if !option || options['l']
-  output_word_count(file_data) if !option || options['w']
-  output_byte_count(file_data) if !option || options['c']
+def output_files_count(file_data, options)
+  output_line_count(file_data) if options['l']
+  output_word_count(file_data) if options['w']
+  output_byte_count(file_data) if options['c']
   output_file(file_data)
 end
 
@@ -70,7 +70,7 @@ def output_file(file_data)
   end
 end
 
-def output_multi_file_options_count(files, options, option)
+def output_multi_file_options_count(files, options)
   total_lines = 0
   total_words = 0
   total_bytes = 0
@@ -79,17 +79,17 @@ def output_multi_file_options_count(files, options, option)
     line_count = line_count(file_content)
     word_count = word_count(file_content)
     byte_count = byte_count(file_content)
-    print line_count.to_s.rjust(4) if !option || options['l']
-    print word_count.to_s.rjust(4) if !option || options['w']
-    print byte_count.to_s.rjust(6) if !option || options['c']
+    print line_count.to_s.rjust(4) if options['l']
+    print word_count.to_s.rjust(4) if options['w']
+    print byte_count.to_s.rjust(6) if options['c']
     print "#{file}\n".to_s.rjust(8)
     total_lines += line_count
     total_words += word_count
     total_bytes += byte_count
   end
-  print total_lines.to_s.rjust(4) if !option || options['l']
-  print total_words.to_s.rjust(4) if !option || options['w']
-  print total_bytes.to_s.rjust(6) if !option || options['c']
+  print total_lines.to_s.rjust(4) if options['l']
+  print total_words.to_s.rjust(4) if options['w']
+  print total_bytes.to_s.rjust(6) if options['c']
   print "total\n".to_s.rjust(8)
 end
 
