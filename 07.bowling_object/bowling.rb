ab794db2a799b11a2ddc9af58cbcd18466b4128f
moegi29 <sedmikrasky0209@gmail.com>
2024-06-13 19:43:54 +0900

    bowling.rbを戻した

--- 07.bowling_object/bowling.rb
+++ 07.bowling_object/bowling.rb
@@ -1,16 +1,6 @@
 # frozen_string_literal: true
 
 require_relative './game'
-score = ARGV[0]
-scores = score.split(',')
-shots = []
-scores.each do |s|
-  if s == 'X'
-    shots << 10
-    shots << 0
-  else
-    shots << s.to_i
-  end
-end
-game = Game.new(shots)
+shot_instances = ARGV[0].split(',')
+game = Game.new(shot_instances)
 puts game.score
