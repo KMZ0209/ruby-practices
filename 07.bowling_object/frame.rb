d3bd1db20adda40c15873e3e09ddb48d7c2f9f08
moegi29 <sedmikrasky0209@gmail.com>
2024-06-18 23:13:49 +0900

    Frameクラス内initializeでshotオブジェクトを渡す,scoreメソッド要素の計算方法修正

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -4,14 +4,14 @@ require_relative './shot'
 class Frame
   attr_reader :first_shot, :second_shot, :third_shot
 
-  def initialize(first_mark, second_mark, third_mark = nil)
-    @first_shot = Shot.new(first_mark)
-    @second_shot = Shot.new(second_mark)
-    @third_shot = Shot.new(third_mark)
+  def initialize(first_shot, second_shot, third_shot = nil)
+    @first_shot = first_shot
+    @second_shot = second_shot
+    @third_shot = third_shot
   end
 
   def score
-    [@first_shot.score, @second_shot.score, @third_shot.score].sum
+    [@first_shot, @second_shot, @third_shot].compact.map(&:score).sum
   end
 
   def strike?
